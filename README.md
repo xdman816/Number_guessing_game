d868b2d8a2f08d3f7bb66150ae3f20154f01fa50
xdman816 <93176229+xdman816@users.noreply.github.com>
2021-10-27 11:07:53 -0400

    Changes
    moved instructions to README
    changed 'high-score' to 'best-score'

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Number_guessing_game
- treehouse data analysis - project #1
+treehouse data analysis - project #1
+When the program starts, we want to:
+    ------------------------------------
+    1. Display an intro/welcome message to the player.
+    2. Store a random number as the answer/solution.
+    3. Continuously prompt the player for a guess.
+      a. If the guess greater than the solution, display to the player "It's lower".
+      b. If the guess is less than the solution, display to the player "It's higher".
+    
+    4. Once the guess is correct, stop looping, inform the user they "Got it"
+         and show how many attempts it took them to get the correct number.
+    5. Save their attempt number to a list.
+    6. At the end of the game, show the player, 1) their number of attempts, 2) the mean, median, and mode of the saved attempts list.
+    7. Ask the player if they want to play again.
--- guessing_game.py
+++ guessing_game.py
@@ -1,21 +1,11 @@
-"""
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project you can use Workspaces. 
-
-NOTE: If you prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
-import random as r 
+import random as rand 
 import statistics as stats
 
 saved_attempt_list = []
 
 def guessing_game():
 
-  answer = r.randint(1, 100)
+  answer = rand.randint(1, 100)
   attempt_no = 1
   attempt_list = []
   while True: 
@@ -38,9 +28,8 @@ def guessing_game():
             attempt_list.append(attempt_no)
             continue 
       except ValueError:
-          print("That is not a valid number. Please pick a number between 1 and 100")
+          print("That is not a valid number! Please pick a number between 1 and 100.")
           continue   
-
 
   print(f"""You got it! The number was {answer}.
 
@@ -50,9 +39,8 @@ def guessing_game():
         Average number of guesses: {stats.mean(saved_attempt_list)}
         50th percentile of guesses: {stats.median(saved_attempt_list)}
         Most common number of guesses: {stats.mode(saved_attempt_list)}
-        All-time high score: {min(saved_attempt_list)} guesses
+        All-time best score: {min(saved_attempt_list)} guesses
         """)
-
 
 def start_game():
 
@@ -70,37 +58,9 @@ def start_game():
             print("Great! The game has reset with a new number \n")
             continue
       
-
-
-
-
 start_game()
 
           
 
 
   
-"""Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Save their attempt number to a list.
-    6. At the end of the game, show the player, 1) their number of attempts, 2) the mean, median, and mode of the saved attempts list.
-    7. Ask the player if they want to play again.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
-
-
-# Kick off the program by calling the start_game function.
-#start_game()
